2e5ddf1ce0692f7fba0744ae29b9132e2ac71998
Daniel Son <sondaniel96@gmail.com>
2016-06-02 21:43:23 -0700

    changed port listener

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var port = process.env.PORT || 8080;
+
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
@@ -20,6 +22,6 @@ io.on('connection', function(socket){
 });
 
 
-http.listen(3000, function(){
-	console.log('listening on *:3000');
+http.listen(port, function(){
+	console.log('Our app is running on http://localhost:' + port);
 });
